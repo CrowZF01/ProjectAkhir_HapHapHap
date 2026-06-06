fa0d3469190284bdab2436255df1bc3f7da3b6d9
71241153_Felix_Benedictus_Setiawan <felixbenedictus00@gmail.com>
2026-06-06 14:29:33 +0700

    update bagian

--- ProjectAkhir_HapHapHap/src/main/java/controller/itemResepController.java
+++ ProjectAkhir_HapHapHap/src/main/java/controller/itemResepController.java
@@ -1,4 +1,46 @@
 package controller;
 
-public class itemResepController    {
+import service.RecipeService;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.Resep;
+import util.imageUtil;
+import util.sessionManager;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import java.util.Optional;
+
+public class itemResepController {
+
+    @FXML
+    private VBox cardUtama;
+
+    @FXML
+    private Label judulLabel, kategoriLabel, bahanLabel, waktuLabel, porsiLabel;
+
+    @FXML
+    private ImageView fotoResep;
+
+    @FXML
+    private Label ikonHati;
+
+    @FXML
+    private javafx.scene.control.Button btnEditAdmin;
+
+    @FXML
+    private javafx.scene.control.Button btnHapusAdmin;
+
+    private Resep resepAktif;
+    private exploreController parentController;
 }
