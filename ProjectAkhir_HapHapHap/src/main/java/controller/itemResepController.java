b98b23d86c99b5835ab13626e84a6c6a4802d7c8
71241153_Felix_Benedicstus_Setiawan <felixbenedictus00@gmail.com>
2026-06-08 13:22:36 +0700

    .

--- ProjectAkhir_HapHapHap/src/main/java/controller/itemResepController.java
+++ ProjectAkhir_HapHapHap/src/main/java/controller/itemResepController.java
@@ -43,4 +43,96 @@ public class itemResepController {
 
     private Resep resepAktif;
     private exploreController parentController;
+
+
+    public void setData(Resep resep) {
+        this.resepAktif = resep;
+
+        judulLabel.setText(resep.getJudul());
+
+        String kategori = resep.getJenisMakanan();
+        if (kategori != null) {
+            kategoriLabel.setText(kategori.toUpperCase());
+        } else {
+            kategoriLabel.setText("UMUM");
+        }
+
+        if (resep.getBahan() != null) {
+            bahanLabel.setText(resep.getBahan());
+        } else {
+            bahanLabel.setText("");
+        }
+
+        waktuLabel.setText("⏱ " + resep.getEstimasiWaktu() + "m");
+        porsiLabel.setText("🍽 " + resep.getPorsiSajian() + " Porsi");
+
+        Image img = imageUtil.getImage(resep.getFoto());
+        if (img != null) {
+            fotoResep.setImage(img);
+        } else {
+            fotoResep.setImage(null);
+        }
+
+        // Mengatur kursor dan event klik untuk card resep
+        cardUtama.setCursor(Cursor.HAND);
+
+        cardUtama.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                handleLihatDetail(event);
+            }
+        });
+
+        if (sessionManager.isLogin() && ikonHati != null) {
+            int idUser = sessionManager.getUser().getId();
+            boolean isFavorit = RecipeService.getInstance().cekFavorit(idUser, resep.getIdResep());
+
+            if (isFavorit) {
+                ikonHati.setText("♥");
+                ikonHati.setStyle("-fx-text-fill: #E74C3C; -fx-font-size: 18px;");
+            } else {
+                ikonHati.setText("♡");
+                ikonHati.setStyle("-fx-text-fill: #333333; -fx-font-size: 18px;");
+            }
+        }
+
+        // Tampilkan tombol edit & hapus hanya jika user aktif adalah ADMIN
+        if (sessionManager.isLogin() && "ADMIN".equalsIgnoreCase(sessionManager.getUser().getRole())) {
+            btnEditAdmin.setVisible(true);
+            btnEditAdmin.setManaged(true);
+            if (btnHapusAdmin != null) {
+                btnHapusAdmin.setVisible(true);
+                btnHapusAdmin.setManaged(true);
+            }
+        } else {
+            btnEditAdmin.setVisible(false);
+            btnEditAdmin.setManaged(false);
+            if (btnHapusAdmin != null) {
+                btnHapusAdmin.setVisible(false);
+                btnHapusAdmin.setManaged(false);
+            }
+        }
+    }
+
+    public void handleLihatDetail(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/felix_71241153/app/copy_Teletubies_haphaphap/detail.fxml")
+            );
+
+            Parent root = loader.load();
+
+            detailController controller = loader.getController();
+            controller.setResepData(resepAktif);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            System.out.println("Gagal membuka halaman detail!");
+            e.printStackTrace();
+        }
+    }
+
 }
